c1f3c9f15b54fa9c2c7036b32bf7140d3f86f73c
khoihuynh300 <khoihuynh300@gmail.com>
2023-04-03 11:21:27 +0700

    them api hien thi hinh anh

---
+++ api/src/main/java/com/example/api/api/imagesAPI.java
@@ -0,0 +1,34 @@
+package com.example.api.api;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class imagesAPI {
+	@GetMapping("upload/{imageName:.+}")
+	@ResponseBody
+	public ResponseEntity<byte[]> getImage(@PathVariable String imageName) throws IOException {
+	    InputStream imageStream = new FileInputStream(new File("D:\\image\\" + imageName));
+	    byte[] imageBytes = IOUtils.toByteArray(imageStream);
+
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.IMAGE_PNG);
+	    headers.setContentLength(imageBytes.length);
+
+	    return new ResponseEntity<byte[]>(imageBytes, headers, HttpStatus.OK);
+	}
+}
--- api/src/main/java/com/example/api/api/test.java
+++ api/src/main/java/com/example/api/api/test.java
@@ -1,15 +1,26 @@
 package com.example.api.api;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api.entity.Users;
-import com.example.api.repository.UsersRepository;
 import com.example.api.service.IUsersService;
 
 @RestController
@@ -30,15 +41,29 @@ public class test {
 		return "test2";
 	}
 	
-	 @GetMapping("/test2")
-	    public Map<String, Object> getResponse() {
-	        Users user = new Users();
-	        user.setFullname("test2");
-			user.setEmail("test@gmail.com");
-			
-	        Map<String, Object> response = new HashMap<>();
-	        response.put("message", "m");
-	        response.put("user", user);
-	        return response;
-	    }
+	@GetMapping("/test2")
+    public Map<String, Object> getResponse() {
+        Users user = new Users();
+        user.setFullname("test2");
+		user.setEmail("test@gmail.com");
+		
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "m");
+        response.put("user", user);
+        return response;
+    }
+	
+	@GetMapping("/getImage2")
+	@ResponseBody
+	public ResponseEntity<byte[]> getImage2() throws IOException {
+	    InputStream imageStream = new FileInputStream(new File("D:\\image\\bgNight.png"));
+	    byte[] imageBytes = IOUtils.toByteArray(imageStream);
+
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.IMAGE_PNG);
+	    headers.setContentLength(imageBytes.length);
+
+	    return new ResponseEntity<byte[]>(imageBytes, headers, HttpStatus.OK);
+	}
+	 
 }
